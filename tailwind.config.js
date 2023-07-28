b1e6d06bd63ea7248019f71c7d4313de987ebf90
abdullah565 <abdullah.rafique565@gmail.com>
2023-07-28 12:55:51 +0500

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    fontSize: {
+      hist_title: "32px",
+      hist_title_lg: "76px",
+      hist_description: "16px",
+      hist_description_lg: "20px",
+    },
+    colors: {
+      hist_red: "#FE490C",
+      hist_black: "#161616",
+      hist_white: {
+        100: "#ffffff1a",
+        500: "#ffffff80",
+        800: "#ffffffcc",
+        900: "#ffffff",
       },
     },
   },
   plugins: [],
-}
+};
+// written here for quick look
+// screens: {
+//   'sm': '640px',
+//   // => @media (min-width: 640px) { ... }
+
+//   'md': '768px',
+//   // => @media (min-width: 768px) { ... }
+
+//   'lg': '1024px',
+//   // => @media (min-width: 1024px) { ... }
+
+//   'xl': '1280px',
+//   // => @media (min-width: 1280px) { ... }
+
+//   '2xl': '1536px',
+//   // => @media (min-width: 1536px) { ... }
+// }
