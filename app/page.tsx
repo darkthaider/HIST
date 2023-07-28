614dc3b1dac830748d8fd64ac59498685212dbba
shaheryar <57302246+xbugfieldx@users.noreply.github.com>
2023-07-28 20:17:40 +0500

    Experience and services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Experience from "@/components/ui/experience/Experience";
+import Services from "@/components/ui/services/Services";
+
 export default function Home() {
   return (
-    <main className="hist-outer-container bg-hist_red">
+    <main className="hist-outer-container bg-hist_re">
       <h1 className="hist-inner-container lg:text-hist_title_lg text-hist_white-900 bg-hist_white-800">
         HIST
       </h1>
+      <Experience />
+      <Services />
     </main>
   );
 }
---
+++ components/ui/experience/Experience.tsx
@@ -0,0 +1,28 @@
+const Experience = () => {
+  return (
+    <section className="hist-outer-container vertical-padding-32-82">
+      <div className="hist-inner-container px-2">
+        <div className="desc-16-20 text-hist_white-500 flex capitalize">
+          <span className="subtitle-rectangle"></span>
+          services
+        </div>
+        <h2 className="title-32-76 text-hist_white-900 sm:max-w-lg max-w-xs">
+          With 30+ years of experience we position companies to become
+          Monopolies.
+        </h2>
+        <div className="flex justify-end" style={{ marginTop: "242px" }}>
+          <p className="text-hist_white-800 max-w-md desc-16-20">
+            Our services include market research, product positioning and
+            branding, distribution network management, regulatory compliance,
+            and much more. We understand that expanding to new markets can be
+            challenging, which is why we work closely with our clients to ensure
+            that their products are well received and reach their target
+            audience. With our expertise, we help our clients overcome any
+            obstacles and achieve success in the Middle East.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Experience;
